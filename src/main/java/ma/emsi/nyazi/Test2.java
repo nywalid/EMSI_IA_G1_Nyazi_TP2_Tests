20b229bbb67f09e429411cca83f8ea41664652cd
walid <walidnyazi17@gmail.com>
2025-11-03 10:21:39 +0100

    Ajout du Test 2 NYAZI

---
+++ src/main/java/ma/emsi/nyazi/Test2.java
@@ -0,0 +1,28 @@
+package ma.emsi.nyazi;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
+
+import java.util.Map;
+
+public class Test2 {
+    public static void main(String[] args) {
+        String cle= System.getenv("GEMINI_KEY");
+        ChatModel modele = GoogleAiGeminiChatModel
+                .builder()
+                .apiKey(cle)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.7)
+                .build();
+        PromptTemplate template = PromptTemplate.from("""
+                Traduis le texte suivant en anglais : {{texte}}""");
+
+        Prompt prompt = template.apply(Map.of(
+                "texte", "Jai un cours demain"
+        ));
+        String reponse = modele.chat(prompt.text());
+        System.out.println(reponse);
+    }
+}
